1f8b254a72f82c420658651408638360542a00c4
Vivaan Baid <baidvivaan782@gmail.com>
2022-08-15 02:26:55 -0400

    Initial Commit

---
+++ PartyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PartyApp
+//
+//  Created by Vivaan Baid on 15/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PartyApp/PartyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyAppApp.swift
+//  PartyApp
+//
+//  Created by Vivaan Baid on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PartyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
